90ee4e0fd8447402fc48f07e5f9a3ade904b75a8
Hasan Alasker <hasanalasker2004@gmail.com>
2025-12-02 20:55:03 +0300

    addded backend basic structure

---
+++ Server/index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+
+const port = process.env.PORT || 4000;
+
+if (!process.env.JWT_SECRET) {
+  console.log("fatal error, no jwt defined");
+}
+
+mongoose
+  .connect(process.env.DATABASE_URL)
+  .then(() => console.log("Connected to mongoDB... ✅"))
+  .catch((err) =>
+    console.log("Error connecting to mongoDB... ❌", err.message)
+  );
+
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`Server running on ${port} 🌍`);
+  console.log(`Accessible at http://YOUR_IP:${port} 🖥️`);
+});
