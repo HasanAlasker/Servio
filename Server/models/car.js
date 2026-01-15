760e17be612e364b37839ef3dcffa429af57bb65
Hasan Alasker <hasanalasker2004@gmail.com>
2026-01-15 15:10:22 +0300

    carMake schema, and car schema

--- Server/models/car.js
+++ Server/models/car.js
@@ -1,6 +1,46 @@
 import mongoose from "mongoose";
 
-const carSchema = new mongoose.Schema({});
+const carSchema = new mongoose.Schema(
+  {
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    make: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    model: {
+      type: Number,
+      min: [1900, "Year must be 1900 or later"],
+      max: [new Date().getFullYear() + 1, "Year cannot be in the future"],
+      required: true,
+    },
+    plateNumber: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    mileage: {
+      type: Number,
+      min: [0, "Mileage cannot be negative"],
+      required: true,
+    },
+    isDeleted: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
 
 const CarModel = mongoose.model("Car", carSchema);
 export default CarModel;
