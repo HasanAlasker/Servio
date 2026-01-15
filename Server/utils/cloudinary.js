f99980e3f762f7d78febc5a0586bc556088e002f
Hasan Alasker <hasanalasker2004@gmail.com>
2026-01-15 10:32:59 +0300

    added utils for image uploads, pushNotification. and a .env.example for collaboration

---
+++ Server/utils/cloudinary.js
@@ -0,0 +1,19 @@
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
+
+const deleteImageFromCloudinary = async (publicId) => {
+  try {
+    const result = await cloudinary.uploader.destroy(publicId);
+    return result;
+  } catch (error) {
+    console.error("Cloudinary delete error:", error);
+    throw error;
+  }
+};
+
+export default deleteImageFromCloudinary;
