453fcdd1583c74c9955f796e7e059116525e3a03
Hasan Alasker <hasanalasker2004@gmail.com>
2026-01-15 15:34:46 +0300

    part model

--- Server/models/part.js
+++ Server/models/part.js
@@ -1,6 +1,60 @@
 import mongoose from "mongoose";
 
-const partSchema = new mongoose.Schema({});
+const partSchema = new mongoose.Schema(
+  {
+    car: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Car",
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      enum: [
+        "engine oil",
+        "oil filter",
+        "air filter",
+        "brake pads",
+        "brake fluid",
+        "coolant",
+        "transmission fluid",
+        "spark plugs",
+        "battery",
+        "tires",
+        "timing belt",
+        "wiper blades",
+      ],
+    },
+    recommendedChangeInterval: {
+      months: {
+        type: Number,
+        min: 0,
+      },
+      miles: {
+        type: Number,
+        min: 0,
+      },
+    },
+    lastChangeDate: {
+      type: Date,
+      required: true,
+      validate: {
+        validator: function (v) {
+          return v <= new Date();
+        },
+        message: "Last change date cannot be in the future",
+      },
+    },
+    lastChangeMileage: {
+      type: Number,
+      required: true,
+      min: [0, "Mileage cannot be negative"],
+    },
+    isDeleted: {},
+  },
+  { timestamps }
+);
 
 const PartModel = mongoose.model("Part", partSchema);
 export default PartModel;
--- Server/models/service.js
+++ Server/models/service.js
@@ -1,6 +1,12 @@
 import mongoose from "mongoose";
 
-const serviceSchema = new mongoose.Schema({});
+const serviceSchema = new mongoose.Schema({
+  car: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Car",
+    required: true,
+  },
+});
 
 const ServiceModel = mongoose.model("Service", serviceSchema);
 export default ServiceModel;
