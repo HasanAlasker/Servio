138930ff8fb643b38d0103741d67d743ef4c53dc
Hasan Alasker <hasanalasker2004@gmail.com>
2026-01-15 15:44:10 +0300

    upcomming service model

---
+++ Server/models/upcomingService.js
@@ -0,0 +1,37 @@
+import mongoose from "mongoose";
+
+const upcomingServiceSchema = new mongoose.Schema({
+  car: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Car",
+    required: true,
+  },
+  customer: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  parts: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Car",
+      required: true,
+    },
+  ],
+  dueBy: {
+    date: {
+      type: Date,
+      index: true,
+    },
+    mileage: {
+      type: Number,
+      min: 0,
+    },
+  },
+});
+
+const UpcomingServiceModel = mongoose.model(
+  "UpcomingService",
+  upcomingServiceSchema
+);
+export default UpcomingServiceModel;
