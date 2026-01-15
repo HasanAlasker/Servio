883aa585774f5bfa5357496d0a34cdeb4bc3f895
Hasan Alasker <hasanalasker2004@gmail.com>
2026-01-15 23:48:46 +0300

    get car-make route

--- Server/routes/cars.js
+++ Server/routes/cars.js
@@ -1,11 +1,27 @@
 import express from "express";
 import mongoose from "mongoose";
+import CarMakeModel from "../models/carMake.js";
 
 const router = express.Router();
 
 // get all
 
 // get mine
+
+// get name&model
+router.get("/car-make", async (req, res) => {
+  try {
+    const carsData = await CarMakeModel.find();
+    return res.status(200).json({ success: true, data: carsData });
+
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      success: false,
+      message: "Server Error",
+    });
+  }
+});
 
 // add car
 
