e071a283fa5e61c4ebf03e988e95d00438fe96eb
Hasan Alasker <hasanalasker2004@gmail.com>
2026-01-20 21:25:10 +0300

    wakeup server endpoint

--- Client/Servio/api/upcomingService.js
+++ Client/Servio/api/upcomingService.js
@@ -3,3 +3,5 @@ import { apiClient } from "./client";
 const endpoint = "/upcomingServices";
 
 export const getUpcomingServices = () => apiClient.get(`${endpoint}/`);
+
+export const isServerAwake = () => apiClient.get(`${endpoint}/wake-up`)
--- Server/routes/upcomingServices.js
+++ Server/routes/upcomingServices.js
@@ -35,4 +35,16 @@ router.get("/", auth, async (req, res) => {
   }
 });
 
+router.get("/wake-up", async (req, res) => {
+  try {
+    return res.status(200).json({ success: true, message: "Awake" });
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      success: false,
+      message: "Server Error",
+    });
+  }
+});
+
 export default router;
