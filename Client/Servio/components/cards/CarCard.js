4addd1049030776d7f86fd271d81937236fcde9d
Hasan Alasker <hasanalasker2004@gmail.com>
2026-01-23 18:05:05 +0300

    car image

--- Client/Servio/components/cards/CarCard.js
+++ Client/Servio/components/cards/CarCard.js
@@ -8,16 +8,18 @@ import SquareInfo from "./SquareInfo";
 import { capFirstLetter } from "../../functions/CapFirstLetterOfWord";
 
 function CarCard({ image, make, name, model, plateNumber, color, mileage }) {
-
   return (
     <CardComp style={styles.container}>
-      {image && <Image src={image} />}
+      {image && <Image style={styles.image} source={{ uri: image }} />}
       <View style={styles.textCont}>
         <RowCont>
           <MText>{capFirstLetter(make)}</MText>
           <MText>{capFirstLetter(name)}</MText>
         </RowCont>
         <SText color={"sec_text"}>{plateNumber}</SText>
+        {/* <MText thin style={{ marginVertical: 10 }}>
+          {model}
+        </MText> */}
         <RowCont gap={25} style={{ marginTop: 20 }}>
           <SquareInfo
             icon={"gauge"}
@@ -31,7 +33,7 @@ function CarCard({ image, make, name, model, plateNumber, color, mileage }) {
             icon={"palette-outline"}
             color={"pink"}
             title={"Color"}
-            text={color}
+            text={capFirstLetter(color)}
             fliped
           />
         </RowCont>
@@ -44,10 +46,16 @@ const styles = StyleSheet.create({
   container: {
     paddingVertical: 0,
     paddingHorizontal: 0,
+    overflow: "hidden",
   },
   textCont: {
     paddingHorizontal: 22,
     paddingVertical: 25,
+  },
+  image: {
+    width: "100%",
+    height: 240,
+    objectFit: "cover",
   },
 });
 
--- Client/Servio/components/text/MText.js
+++ Client/Servio/components/text/MText.js
@@ -3,7 +3,7 @@ import AppText from "../../config/AppText";
 import useThemedStyles from "../../hooks/useThemedStyles";
 import { useTheme } from "../../context/ThemeContext";
 
-function MText({ children, color, thin, faded }) {
+function MText({ children, color, thin, faded, style }) {
   const { theme } = useTheme();
   const styles = useThemedStyles(getStyles);
   return (
@@ -12,6 +12,7 @@ function MText({ children, color, thin, faded }) {
         styles.text,
         { color: theme[color] || theme["main_text"] },
         { fontWeight: thin || "bold" },
+        style
       ]}
     >
       {children}
