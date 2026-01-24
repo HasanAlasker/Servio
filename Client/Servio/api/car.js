3b7d74577d1453e47516eb86fc926e21fe96777b
Hasan Alasker <hasanalasker2004@gmail.com>
2026-01-24 11:27:46 +0300

    added contentType multipart/form-data in fornt endpoits for adding car and editting

--- Client/Servio/api/car.js
+++ Client/Servio/api/car.js
@@ -10,13 +10,21 @@ export const getMyCars = () => apiClient.get(`${endpoint}/mine`);
 
 export const getCarById = (id) => apiClient.get(`${endpoint}/${id}`);
 
-export const addCar = (data) => apiClient.post(`${endpoint}/add`, data);
+export const addCar = (data) =>
+  apiClient.post(`${endpoint}/add`, data, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
 
 export const editCar = (id, data) =>
-  apiClient.patch(`${endpoint}/edit/${id}`, data);
+  apiClient.patch(`${endpoint}/edit/${id}`, data, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
 
 export const updateMileage = (id, data) =>
   apiClient.patch(`${endpoint}/mileage/${id}`, data);
 
-export const deleteCar = (id) =>
-  apiClient.patch(`${endpoint}/delete/${id}`);
+export const deleteCar = (id) => apiClient.patch(`${endpoint}/delete/${id}`);
