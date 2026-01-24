df92ee0524335876a39ef71e9523cd05708ac4c9
Hasan Alasker <hasanalasker2004@gmail.com>
2026-01-24 18:33:09 +0300

    fetching appointments

--- Client/Servio/screens/carOwner/Bookings.js
+++ Client/Servio/screens/carOwner/Bookings.js
@@ -1,10 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { View, StyleSheet } from "react-native";
 import Navbar from "../../components/general/Navbar";
 import FullScreen from "../../components/general/FullScreen";
 import SafeScreen from "../../components/general/SafeScreen";
+import useApi from "../../hooks/useApi";
+import {
+  getPastAppointments,
+  getUpcomingAppointments,
+} from "../../api/appointment";
 
 function Bookings(props) {
+  const [upcoming, setUpcoming] = useState([]);
+  const [past, setPast] = useState([]);
+
+  const {
+    data: fetchedUpcoming,
+    request: fetchUpcoming,
+    loading: loadingUpcoming,
+  } = useApi(getUpcomingAppointments);
+
+  const {
+    data: fetchedPast,
+    request: fetchPast,
+    loading: loadingPast,
+  } = useApi(getPastAppointments);
+
+  useEffect(() => {
+    fetchUpcoming();
+    fetchPast();
+  }, []);
+
+  useEffect(() => {
+    setUpcoming(fetchedUpcoming);
+    setPast(fetchedPast);
+  }, [fetchUpcoming, fetchedPast]);
+
   return (
     <SafeScreen>
       <View style={styles.container}></View>
