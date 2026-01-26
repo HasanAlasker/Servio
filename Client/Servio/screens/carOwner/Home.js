d35328123d1350c2b0d9fdd56ae4fa75d56a58fb
Hasan Alasker <hasanalasker2004@gmail.com>
2026-01-26 09:19:02 +0300

    data in home fetched properly

--- Client/Servio/screens/carOwner/Home.js
+++ Client/Servio/screens/carOwner/Home.js
@@ -9,9 +9,25 @@ import CardLeftBorder from "../../components/cards/CardLeftBorder";
 import GapContainer from "../../components/general/GapContainer";
 import SquareHome from "../../components/cards/SquareHome";
 import { useNavigation } from "@react-navigation/native";
+import useApi from "../../hooks/useApi";
+import { countDocs } from "../../api/user";
+import { useEffect } from "react";
 
 function Home(props) {
   const navigaiton = useNavigation();
+
+  const {
+    data: fetchedDocs,
+    request: fetchDocs,
+    loading,
+    error,
+  } = useApi(countDocs);
+
+  useEffect(() => {
+    fetchDocs();
+  }, []);
+
+  const data = fetchedDocs;
 
   return (
     <SafeScreen>
@@ -20,15 +36,15 @@ function Home(props) {
         <GapContainer style={styles.container}>
           <CardLeftBorder
             title={"Number of cars: "}
-            data={2}
+            data={loading ? "..." : data.cars}
           />
           <CardLeftBorder
             title={"Appointments: "}
-            data={1}
+            data={loading ? "..." : data.appointments}
           />
           <CardLeftBorder
             title={"Due services: "}
-            data={2}
+            data={loading ? "..." : data.services}
           />
         </GapContainer>
 
