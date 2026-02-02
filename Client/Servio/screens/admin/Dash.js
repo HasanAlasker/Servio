e1626872bea93caf8309daeb52149ce724da7899
Hasan Alasker <hasanalasker2004@gmail.com>
2026-02-02 19:08:26 +0300

    admin dash renders doc count

--- Client/Servio/screens/admin/Dash.js
+++ Client/Servio/screens/admin/Dash.js
@@ -18,13 +18,52 @@ function Dash(props) {
     request();
   }, [user, error]);
 
-  console.log(data)
+  console.log(data);
 
   return (
     <SafeScreen>
       <ScrollScreen>
         <GapContainer>
-          <CardLeftBorder title={"Active Shops:"} data={data.activeShops} />
+          <CardLeftBorder
+            title={"Shop Requests:"}
+            data={loading ? "..." : data.shopRequests}
+          />
+          <CardLeftBorder
+            title={"Reports:"}
+            data={loading ? "..." : data.reports}
+          />
+          <CardLeftBorder
+            title={"Suggestions:"}
+            data={loading ? "..." : data.suggestions}
+          />
+          <CardLeftBorder
+            title={"Active Users:"}
+            data={loading ? "..." : data.activeUsers}
+          />
+          <CardLeftBorder
+            title={"Car Owners:"}
+            data={loading ? "..." : data.carOwners}
+          />
+          <CardLeftBorder
+            title={"Shop Owners:"}
+            data={loading ? "..." : data.shopOwners}
+          />
+          <CardLeftBorder
+            title={"Deleted Users:"}
+            data={loading ? "..." : data.deletedUsers}
+          />
+          <CardLeftBorder
+            title={"Active Shops:"}
+            data={loading ? "..." : data.activeShops}
+          />
+          <CardLeftBorder
+            title={"Deleted Shops:"}
+            data={loading ? "..." : data.deletedShops}
+          />
+          <CardLeftBorder
+            title={"Registered Cars:"}
+            data={loading ? "..." : data.cars}
+          />
         </GapContainer>
       </ScrollScreen>
       <Navbar />
