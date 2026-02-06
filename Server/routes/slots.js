a8ffae682f252804a1a6346ed9c056819e6782b0
Hasan Alasker <hasanalasker2004@gmail.com>
2026-02-06 11:54:53 +0300

    slots

---
+++ Server/routes/slots.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const router = express.Router();
+
+// get busy slots
+
+// chech slot
+
+// delete slot (this happens in appointments route when shopOwner rejects/ or user cancels)
+
+// create slot (this happens in appointments route when user books)
+
+export default router;
