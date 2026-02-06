0b3fc48163902572b4b987ecb58c910d10ad88b4
Hasan Alasker <hasanalasker2004@gmail.com>
2026-02-06 11:46:45 +0300

    basic screen stucture

--- Client/Servio/components/form/AppForm.js
+++ Client/Servio/components/form/AppForm.js
@@ -1,12 +1,19 @@
 import { StyleSheet } from "react-native";
 import { Formik } from "formik";
 
-function AppForm({ children, initialValues, validationSchema, onSubmit }) {
+function AppForm({
+  children,
+  initialValues,
+  validationSchema,
+  onSubmit,
+  enableReinitialize,
+}) {
   return (
     <Formik
       initialValues={initialValues}
       validationSchema={validationSchema}
       onSubmit={onSubmit}
+      enableReinitialize={enableReinitialize}
     >
       {(formikProps) => (
         <>{typeof children === "function" ? children(formikProps) : children}</>
--- Client/Servio/screens/carOwner/MakeAppointment.js
+++ Client/Servio/screens/carOwner/MakeAppointment.js
@@ -1,19 +1,71 @@
 import { View, StyleSheet } from "react-native";
 import SafeScreen from "../../components/general/SafeScreen";
-import ScrollScreen from "../../components/general/ScrollScreen";
 import GapContainer from "../../components/general/GapContainer";
 import KeyboardScrollScreen from "../../components/general/KeyboardScrollScreen";
 import Navbar from "../../components/general/Navbar";
 import { useRoute } from "@react-navigation/native";
+import AppForm from "../../components/form/AppForm";
+import * as Yup from "yup";
+import FormikDatePicker from "../../components/form/FormikDatePicker";
+import SubmitBtn from "../../components/form/SubmitBtn";
+import { useState } from "react";
+
+const validationSchema = Yup.object({
+  date: Yup.date()
+    .required("Please select a date")
+    .min(new Date(), "Date cannot be in the past"),
+  time: Yup.string().required("Please select a time").nullable(),
+});
 
 function MakeAppointment(props) {
+  const [hasBeenSubmited, setHasBeenSubmited] = useState(false);
   const route = useRoute();
   const params = route.params;
+
+  const initialValues = {
+    date: "",
+    time: "",
+  };
+
+  const handleSubmit = async (values) => {
+    // dont forget to format the scheduled date to iso so its date and time
+  };
 
   return (
     <SafeScreen>
       <KeyboardScrollScreen>
-        <GapContainer></GapContainer>
+        <View style={{ marginVertical: "auto" }}>
+          <AppForm
+            initialValues={initialValues}
+            validationSchema={validationSchema}
+            onSubmit={handleSubmit}
+            enableReinitialize={true}
+          >
+            {({ values }) => (
+              <GapContainer gap={15}>
+                <FormikDatePicker
+                  name={"date"}
+                  icon="calendar-outline"
+                  hasBeenSubmitted={hasBeenSubmited}
+                />
+                {values.date && (
+                  <FormikDatePicker
+                    name={"time"}
+                    mode="time"
+                    placeholder="Select time"
+                    icon="clock-outline"
+                    hasBeenSubmitted={hasBeenSubmited}
+                  />
+                )}
+                <SubmitBtn
+                  defaultText="Confirm"
+                  submittingText="Confirming"
+                  setHasBeenSubmitted={setHasBeenSubmited}
+                />
+              </GapContainer>
+            )}
+          </AppForm>
+        </View>
       </KeyboardScrollScreen>
       <Navbar />
     </SafeScreen>
@@ -21,7 +73,9 @@ function MakeAppointment(props) {
 }
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    marginVertical: "auto",
+  },
 });
 
 export default MakeAppointment;
