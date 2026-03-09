731ce72a0f6aa0e0eb8df979d03b03e7db01d47e
Hasan Alasker <hasanalasker2004@gmail.com>
2026-03-09 15:17:42 +0300

    admin dash remake

---
+++ Client/Servio/components/general/AdminDash.js
@@ -0,0 +1,80 @@
+import { StyleSheet } from "react-native";
+import { UseUser } from "../../context/UserContext";
+import CardLeftBorder from "../../components/cards/CardLeftBorder";
+import GapContainer from "../../components/general/GapContainer";
+import useApi from "../../hooks/useApi";
+import { adminCountDocs } from "../../api/user";
+import { useEffect } from "react";
+import MText from "../../components/text/MText";
+
+function AdminDash(props) {
+  const { user } = UseUser();
+  const { data, request, loading, error } = useApi(adminCountDocs);
+
+  useEffect(() => {
+    request();
+  }, [user, error]);
+  return (
+    <GapContainer>
+      <MText thin color={"sec_text"}>
+        Metrics
+      </MText>
+      <CardLeftBorder
+        title={"Shop Requests:"}
+        titleIcon={"store-clock-outline"}
+        data={loading ? "..." : data.shopRequests}
+      />
+      <CardLeftBorder
+        title={"Reports:"}
+        titleIcon={"flag-outline"}
+        data={loading ? "..." : data.reports}
+      />
+      <CardLeftBorder
+        title={"Suggestions:"}
+        titleIcon={"lightbulb-outline"}
+        data={loading ? "..." : data.suggestions}
+      />
+      <CardLeftBorder
+        title={"Active Users:"}
+        titleIcon={"account-check-outline"}
+        data={loading ? "..." : data.activeUsers}
+      />
+      <CardLeftBorder
+        title={"Car Owners:"}
+        titleIcon={"car-outline"}
+        data={loading ? "..." : data.carOwners}
+      />
+      <CardLeftBorder
+        title={"Shop Owners:"}
+        titleIcon={"store-outline"}
+        data={loading ? "..." : data.shopOwners}
+      />
+      <CardLeftBorder
+        title={"Deleted Users:"}
+        titleIcon={"account-remove-outline"}
+        data={loading ? "..." : data.deletedUsers}
+      />
+      <CardLeftBorder
+        title={"Active Shops:"}
+        titleIcon={"storefront-outline"}
+        data={loading ? "..." : data.activeShops}
+      />
+      <CardLeftBorder
+        title={"Deleted Shops:"}
+        titleIcon={"store-remove-outline"}
+        data={loading ? "..." : data.deletedShops}
+      />
+      <CardLeftBorder
+        title={"Registered Cars:"}
+        titleIcon={"car-multiple"}
+        data={loading ? "..." : data.cars}
+      />
+    </GapContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {},
+});
+
+export default AdminDash;
--- Client/Servio/screens/admin/Dash.js
+++ Client/Servio/screens/admin/Dash.js
@@ -1,102 +1,52 @@
 import { StyleSheet } from "react-native";
 import SafeScreen from "../../components/general/SafeScreen";
-import { UseUser } from "../../context/UserContext";
 import OfflineModal from "../../components/general/OfflineModal";
 import Navbar from "../../components/general/Navbar";
 import ScrollScreen from "../../components/general/ScrollScreen";
-import CardLeftBorder from "../../components/cards/CardLeftBorder";
 import GapContainer from "../../components/general/GapContainer";
-import useApi from "../../hooks/useApi";
-import { adminCountDocs } from "../../api/user";
-import { useEffect } from "react";
-import LText from "../../components/text/LText";
 import SquareHome from "../../components/cards/SquareHome";
 import { useNavigation } from "@react-navigation/native";
 import HelloUser from "../../components/general/HelloUser";
 import MText from "../../components/text/MText";
+import RowCont from "../../components/general/RowCont";
+import AdminDash from "../../components/general/AdminDash";
 
 function Dash(props) {
-  const { user } = UseUser();
-  const { data, request, loading, error } = useApi(adminCountDocs);
-
   const navigate = useNavigation();
-
-  useEffect(() => {
-    request();
-  }, [user, error]);
 
   return (
     <SafeScreen>
       <ScrollScreen>
-        <HelloUser />
-        <MText thin color={"sec_text"}>
-          Quick Actions
-        </MText>
-        <GapContainer style={[styles.container, styles.row]}>
-          <SquareHome
-            color={"lightBlue"}
-            icon={"store-remove-outline"}
-            title={"Deleted"}
-            onPress={() => navigate.navigate("DeletedShops")}
-          />
-          <SquareHome
-            color={"green"}
-            icon={"chat-outline"}
-            title={"Received"}
-            onPress={() => navigate.navigate("SeeSuggestions")}
-          />
+        <GapContainer gap={40}>
+          <HelloUser />
 
-          <SquareHome
-            color={"pink"}
-            icon={"close-circle-outline"}
-            title={"Closed"}
-            onPress={() => navigate.navigate("Reports")}
-          />
-        </GapContainer>
-        <MText thin color={"sec_text"}>
-          Metrics
-        </MText>
-        <GapContainer style={styles.container}>
-          <CardLeftBorder
-            title={"Shop Requests:"}
-            data={loading ? "..." : data.shopRequests}
-          />
-          <CardLeftBorder
-            title={"Reports:"}
-            data={loading ? "..." : data.reports}
-          />
-          <CardLeftBorder
-            title={"Suggestions:"}
-            data={loading ? "..." : data.suggestions}
-          />
-          <CardLeftBorder
-            title={"Active Users:"}
-            data={loading ? "..." : data.activeUsers}
-          />
-          <CardLeftBorder
-            title={"Car Owners:"}
-            data={loading ? "..." : data.carOwners}
-          />
-          <CardLeftBorder
-            title={"Shop Owners:"}
-            data={loading ? "..." : data.shopOwners}
-          />
-          <CardLeftBorder
-            title={"Deleted Users:"}
-            data={loading ? "..." : data.deletedUsers}
-          />
-          <CardLeftBorder
-            title={"Active Shops:"}
-            data={loading ? "..." : data.activeShops}
-          />
-          <CardLeftBorder
-            title={"Deleted Shops:"}
-            data={loading ? "..." : data.deletedShops}
-          />
-          <CardLeftBorder
-            title={"Registered Cars:"}
-            data={loading ? "..." : data.cars}
-          />
+          <GapContainer>
+            <MText thin color={"sec_text"}>
+              Quick Actions
+            </MText>
+            <RowCont style={styles.row}>
+              <SquareHome
+                color={"lightBlue"}
+                icon={"store-remove-outline"}
+                title={"Deleted"}
+                onPress={() => navigate.navigate("DeletedShops")}
+              />
+              <SquareHome
+                color={"green"}
+                icon={"chat-outline"}
+                title={"Received"}
+                onPress={() => navigate.navigate("SeeSuggestions")}
+              />
+
+              <SquareHome
+                color={"pink"}
+                icon={"close-circle-outline"}
+                title={"Closed"}
+                onPress={() => navigate.navigate("Reports")}
+              />
+            </RowCont>
+            <AdminDash />
+          </GapContainer>
         </GapContainer>
       </ScrollScreen>
       <Navbar />
