68799d5de377ae824762bb307c96ad71734b7ecb
Hasan Alasker <hasanalasker2004@gmail.com>
2026-03-09 14:50:30 +0300

    home remaking

--- Client/Servio/screens/carOwner/Home.js
+++ Client/Servio/screens/carOwner/Home.js
@@ -10,75 +10,21 @@ import OfflineModal from "../../components/general/OfflineModal";
 import { UseCar } from "../../context/CarContext";
 import { UseService } from "../../context/ServiceContext";
 import { UseAppointment } from "../../context/AppointmentContext";
-import HelloUser from "../../components/general/HelloUser";
 import MText from "../../components/text/MText";
-import { capFirstLetter } from "../../functions/CapFirstLetterOfWord";
 import RowCont from "../../components/general/RowCont";
+import UsersDash from "../../components/general/UsersDash";
 
 function Home(props) {
   const navigaiton = useNavigation();
   const { cars } = UseCar();
   const { countAppointments } = UseAppointment();
-  const { countDueServices, services, loading } = UseService();
-
-  const statusPriority = { overdue: 0, soon: 1, upcoming: 2 };
-
-  const goThrough = () => {
-    const carMap = {};
-
-    for (const service of services) {
-      const carId = service.car._id;
-      if (
-        !carMap[carId] ||
-        statusPriority[service.status] < statusPriority[carMap[carId].status]
-      ) {
-        carMap[carId] = service;
-      }
-    }
-
-    return Object.values(carMap).sort(
-      (a, b) => statusPriority[a.status] - statusPriority[b.status],
-    );
-  };
-  const ServiceList = goThrough().map((s) => (
-    <CardLeftBorder
-      key={s._id}
-      status={s.status}
-      customText={capFirstLetter(s.car.make + " " + s.car.name)}
-      showBtn
-      onPress={() => navigaiton.navigate("Service")}
-    />
-  ));
+  const { countDueServices } = UseService();
 
   return (
     <SafeScreen>
       <ScrollScreen>
         <GapContainer gap={40}>
-          <HelloUser />
-
-          {!loading && ServiceList.length > 0 ? (
-            <GapContainer>
-              <MText
-                thin
-                color={
-                  ServiceList[0].props.status === "soon" ? "orange" : "darkPink"
-                }
-              >
-                {ServiceList[0].props.status === "soon"
-                  ? "Coming Later"
-                  : "Attention Needed"}
-              </MText>
-              {ServiceList}
-            </GapContainer>
-          ) : !loading && ServiceList.length === 0 ? (
-            <CardLeftBorder
-              status={" "}
-              icon={"checkbox-marked-circle-outline"}
-              customColor={"green"}
-              miniTitle={"You're all set"}
-              customText={"No services required!"}
-            />
-          ) : null}
+          <UsersDash />
 
           <GapContainer>
             <MText thin color={"sec_text"}>
@@ -137,9 +83,6 @@ function Home(props) {
 }
 
 const styles = StyleSheet.create({
-  container: {
-    marginVertical: 30,
-  },
   row: {
     flexDirection: "row",
     justifyContent: "space-between",
