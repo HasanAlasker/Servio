6e89384cc0f29cd8b2daf5ce6bfa1d09e4ee8b5c
Hasan Alasker <hasanalasker2004@gmail.com>
2026-04-22 10:01:01 +0300

    fix scroll form on web

--- Client/Servio/components/general/KeyboardScrollScreen.js
+++ Client/Servio/components/general/KeyboardScrollScreen.js
@@ -3,7 +3,20 @@ import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view
 import ScrollScreen from "./ScrollScreen";
 
 function KeyboardScrollScreen({ children, ...other }) {
-  if (Platform.OS === "web") return <ScrollView>{children}</ScrollView>;
+  if (Platform.OS === "web")
+    return (
+      <ScrollView
+        contentContainerStyle={{
+          flex: 1,
+          width: "100%",
+          paddingVertical: 30,
+          maxWidth: 650,
+          marginHorizontal: "auto",
+        }}
+      >
+        {children}
+      </ScrollView>
+    );
   return (
     <KeyboardAwareScrollView
       contentContainerStyle={{ flexGrow: 1, paddingBottom: 30, paddingTop: 20 }}
