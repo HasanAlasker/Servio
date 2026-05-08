92cb150a6e7246a0ea8ab33087cec0ff44f995a1
Hasan Alasker <hasanalasker2004@gmail.com>
2026-05-08 11:57:37 +0300

    better user experience for fetching location only when needed

--- Client/Servio/App.js
+++ Client/Servio/App.js
@@ -191,7 +191,7 @@ const AppNavigator = () => {
     loadCars();
     loadServices();
     loadAppointments();
-    fetchUserLocation();
+    // fetchUserLocation();
   }, []);
 
   useEffect(() => {
